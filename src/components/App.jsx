e082d97d8c4d9e4d5cb2183270e59dec22fcf4ed
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-06-04 13:47:25 +0300

    основа

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <div>
+          <form>
+            <label>
+              <div>Name</div>
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              />
+            </label>
+            <button type="button">Add contact</button>
+          </form>
+        </div>
+        <div>
+          <h2>Contacts</h2>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
