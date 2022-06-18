7c800053859e10033fe2b0dd73ff1c14e3f3e64e
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-06-18 17:22:41 +0300

    хук форми

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import {
   FormContainer,
   InputContainer,
@@ -7,65 +7,54 @@ import {
   Input,
 } from './ContactForm.styled';
 
-export default class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export default function ContactForm({ submitProp }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handlerChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handlerSubmit = e => {
+  const handlerSubmit = e => {
     e.preventDefault();
-    this.props.submitProp(this.state);
-    this.reset();
+    submitProp({ name, number });
+    reset();
   };
 
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <FormContainer>
-        <form onSubmit={this.handlerSubmit}>
-          <InputContainer>
-            <label>
-              <LableText>Ім’я контакту</LableText>
-              <Input
-                type="text"
-                name="name"
-                value={this.state.name}
-                onChange={this.handlerChange}
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-                placeholder="Ім’я Прізвище"
-              />
-            </label>
-            <label>
-              <LableText>Номер</LableText>
-              <Input
-                type="tel"
-                name="number"
-                value={this.state.number}
-                onChange={this.handlerChange}
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-                placeholder="000-00-00"
-              />
-            </label>
-          </InputContainer>
-          <Button type="submit">Додати контакт</Button>
-        </form>
-      </FormContainer>
-    );
-  }
+  return (
+    <FormContainer>
+      <form onSubmit={handlerSubmit}>
+        <InputContainer>
+          <label>
+            <LableText>Ім’я контакту</LableText>
+            <Input
+              type="text"
+              name="name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              placeholder="Ім’я Прізвище"
+            />
+          </label>
+          <label>
+            <LableText>Номер</LableText>
+            <Input
+              type="tel"
+              name="number"
+              value={number}
+              onChange={e => setNumber(e.target.value)}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              placeholder="000-00-00"
+            />
+          </label>
+        </InputContainer>
+        <Button type="submit">Додати контакт</Button>
+      </form>
+    </FormContainer>
+  );
 }
