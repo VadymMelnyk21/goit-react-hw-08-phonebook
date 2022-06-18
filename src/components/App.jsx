eba87717e912c2573aff6a453ed78d7bc128c564
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-06-18 17:01:04 +0300

    хук апп

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import { Container, TitleH, TitleH2 } from './App.styled';
 
-export default class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export default function App() {
+  const contactsDefault = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+  const [contacts, setContacts] = useState([...contactsDefault] ?? []);
+  const [filter, setFilter] = useState('');
 
   // ДЗ 3 збереження в локалсторедж початок
-  componentDidUpdate(prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem(
-        'contactsLocalStorage',
-        JSON.stringify(this.state.contacts)
-      );
-    }
-  }
 
-  componentDidMount() {
+  useEffect(() => {
+    console.log('getItem');
     const contactsLS = localStorage.getItem('contactsLocalStorage');
     const contactsParse = JSON.parse(contactsLS);
 
     if (contactsParse) {
-      this.setState({ contacts: contactsParse });
+      console.log('getcontactsParse', contactsParse);
+      setContacts(contactsParse);
     }
-  }
+  }, []);
+
+  useEffect(() => {
+    console.log('setItem');
+    console.log('setItemcont', contacts);
+    localStorage.setItem('contactsLocalStorage', JSON.stringify(contacts));
+  }, [contacts]);
+
   // ДЗ 3 збереження в локалсторедж кінець
 
-  addContact = ({ name, number }) => {
-    const { contacts } = this.state;
-
+  const addContact = ({ name, number }) => {
+    console.log('addContact');
     const newContact = {
       name,
       number,
@@ -47,43 +46,37 @@ export default class App extends Component {
 
     contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())
       ? alert(`${name} вже в контактах`)
-      : this.setState(({ contacts }) => ({
-          contacts: [newContact, ...contacts],
-        }));
+      : setContacts([newContact, ...contacts]);
   };
 
-  deleteContact = contactId => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    console.log('deleteContact');
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    console.log('changeFilter');
+    setFilter(e.currentTarget.value);
   };
 
-  filterList = () => {
-    const { filter, contacts } = this.state;
+  const filterList = () => {
+    console.log('filterList');
     const normalValue = filter.toLowerCase().trim();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalValue)
     );
   };
 
-  render() {
-    const { filter } = this.state;
-    return (
-      <Container>
-        <TitleH>Телефонна книга</TitleH>
-        <ContactForm submitProp={this.addContact} />
+  return (
+    <Container>
+      <TitleH>Телефонна книга</TitleH>
+      <ContactForm submitProp={addContact} />
 
-        <TitleH2>Контакти</TitleH2>
-        <Filter value={filter} changeFilter={this.changeFilter} />
-        <ContactList
-          contacts={this.filterList()}
-          deleteContact={this.deleteContact}
-        />
-      </Container>
-    );
-  }
+      <TitleH2>Контакти</TitleH2>
+      <Filter value={filter} changeFilter={changeFilter} />
+      <ContactList contacts={filterList()} deleteContact={deleteContact} />
+    </Container>
+  );
 }
