bf79cea4656f6eaaa0211091a95ad50303b42ca6
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-07-09 18:38:27 +0300

    форма

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,65 @@
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
+// import { useState, useEffect } from 'react';
+// import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import { Container, TitleH, TitleH2 } from './App.styled';
-import { contactsDefault } from '../data/contactsDefault';
+// import { contactsDefault } from '../data/contactsDefault';
 
 export default function App() {
-  const [contacts, setContacts] = useState([...contactsDefault] ?? []);
-  const [filter, setFilter] = useState('');
+  // const [contacts, setContacts] = useState([...contactsDefault] ?? []);
+  // const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    const contactsLS = localStorage.getItem('contactsLocalStorage');
-    const contactsParse = JSON.parse(contactsLS);
+  // useEffect(() => {
+  //   const contactsLS = localStorage.getItem('contactsLocalStorage');
+  //   const contactsParse = JSON.parse(contactsLS);
 
-    if (contactsParse) {
-      setContacts(contactsParse);
-    }
-  }, []);
+  //   if (contactsParse) {
+  //     setContacts(contactsParse);
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    localStorage.setItem('contactsLocalStorage', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('contactsLocalStorage', JSON.stringify(contacts));
+  // }, [contacts]);
 
-  const addContact = ({ name, number }) => {
-    const newContact = {
-      name,
-      number,
-      id: nanoid(),
-    };
+  // const addContact = ({ name, number }) => {
+  //   const newContact = {
+  //     name,
+  //     number,
+  //     id: nanoid(),
+  //   };
 
-    contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())
-      ? alert(`${name} вже в контактах`)
-      : setContacts([newContact, ...contacts]);
-  };
+  //   contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())
+  //     ? alert(`${name} вже в контактах`)
+  //     : setContacts([newContact, ...contacts]);
+  // };
 
-  const deleteContact = contactId => {
-    setContacts(prevContacts =>
-      prevContacts.filter(contact => contact.id !== contactId)
-    );
-  };
+  // const deleteContact = contactId => {
+  //   setContacts(prevContacts =>
+  //     prevContacts.filter(contact => contact.id !== contactId)
+  //   );
+  // };
 
-  const changeFilter = e => {
-    setFilter(e.currentTarget.value);
-  };
+  // const changeFilter = e => {
+  //   setFilter(e.currentTarget.value);
+  // };
 
-  const filterList = () => {
-    const normalValue = filter.toLowerCase().trim();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalValue)
-    );
-  };
+  // const filterList = () => {
+  //   const normalValue = filter.toLowerCase().trim();
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(normalValue)
+  //   );
+  // };
 
   return (
     <Container>
       <TitleH>Телефонна книга</TitleH>
-      <ContactForm submitProp={addContact} />
+      <ContactForm />
 
       <TitleH2>Контакти</TitleH2>
-      <Filter value={filter} changeFilter={changeFilter} />
-      <ContactList contacts={filterList()} deleteContact={deleteContact} />
+      {/* <Filter /> */}
+      {/* <ContactList /> */}
     </Container>
   );
 }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,14 +6,35 @@ import {
   Button,
   Input,
 } from './ContactForm.styled';
+import { nanoid } from 'nanoid';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'redux/contactsSlice';
 
-export default function ContactForm({ submitProp }) {
+export default function ContactForm() {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
+  const getContacts = state => state.contscts;
+
+  const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
+
+  const changeName = e => setName(e.target.value);
+  const changeNumber = e => setNumber(e.target.value);
+
   const handlerSubmit = e => {
     e.preventDefault();
-    submitProp({ name, number });
+    // submitProp({ name, number });
+    const newContact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+
+    contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())
+      ? alert(`${name} вже в контактах`)
+      : dispatch(addContact(newContact));
+
     reset();
   };
 
@@ -32,11 +53,12 @@ export default function ContactForm({ submitProp }) {
               type="text"
               name="name"
               value={name}
-              onChange={e => setName(e.target.value)}
+              onChange={changeName}
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
               placeholder="Ім’я Прізвище"
+              autoComplete="off"
             />
           </label>
           <label>
@@ -45,11 +67,12 @@ export default function ContactForm({ submitProp }) {
               type="tel"
               name="number"
               value={number}
-              onChange={e => setNumber(e.target.value)}
+              onChange={changeNumber}
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
               placeholder="000-00-00"
+              autoComplete="off"
             />
           </label>
         </InputContainer>
