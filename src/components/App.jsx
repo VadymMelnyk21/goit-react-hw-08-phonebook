7147dbd63fb0e8ce7641bfff9f68777484d7eb62
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-07-09 20:23:14 +0300

    фільтр

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,7 +58,7 @@ export default function App() {
       <ContactForm />
 
       <TitleH2>Контакти</TitleH2>
-      {/* <Filter /> */}
+      <Filter />
       <ContactList />
     </Container>
   );
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,22 +1,23 @@
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFilter, getFilter } from 'redux/contactsSlice';
 import { FilterContainer, Input, Text } from './Filter.styled';
 
-export default function Filter({ value, changeFilter }) {
+export default function Filter() {
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  const handleChangeFilter = e => dispatch(changeFilter(e.target.value));
+
   return (
     <FilterContainer>
       <Text>Пошук контакту за іменем</Text>
       <Input
         type="text"
         name="filter"
-        value={value}
-        onChange={changeFilter}
+        value={filter}
+        onChange={handleChangeFilter}
         placeholder="Ім’я Прізвище"
       />
     </FilterContainer>
   );
 }
-
-Filter.propTypes = {
-  value: PropTypes.string.isRequired,
-  changeFilter: PropTypes.func.isRequired,
-};
