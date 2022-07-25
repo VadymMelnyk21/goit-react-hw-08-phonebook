8c8852c9003dd9e7fe512e63bd7939e4737a41fb
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-07-26 00:10:13 +0300

    збереження позиції при перезавантаженні

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,13 +1,27 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
 import { getIsLoggedIn } from 'redux/authorization/authorization-selectors';
+import {
+  Container,
+  Link,
+  Text,
+  Title,
+} from '../../pages/HomePage/HomePage.styled';
 
 export default function PrivateRoute({ children }) {
   const isLoggedIn = useSelector(getIsLoggedIn);
 
   return (
     <>
-      {!isLoggedIn && <Navigate to="/" replace />}
+      {!isLoggedIn && (
+        <Container>
+          <Title>Вітаємо у книзі контактів</Title>
+          <Text>
+            Щоб отримати доступ до ваших контактів <br />
+            будь ласка <Link to="/login">авторизуйтеся</Link>
+          </Text>
+        </Container>
+      )}
+
       {isLoggedIn && children}
     </>
   );
